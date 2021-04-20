c7a234e5e279b7e5b6d6205ea23a1c1bee8715cf
rafaelib <rafaeliunes97@gmail.com>
2021-04-20 05:26:36 -0300

    tela de criar perguntas parcialmente adicionada

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,84 @@
 let controleValidacaoInfos = true; // quando tem o valor de true, as informações são válidas
 
-function informarInfosQuiz(){
+function enviarInfosQuiz(){
     const pagQuiz = document.querySelector('.pagina-criando-quiz');
-    pagQuiz.classList.toggle('escondido');
+    pagQuiz.classList.remove('escondido');
     pagQuiz.innerHTML = `
     <div class="container-novo-quiz">
             <h2 class="instrucoes-criando-quiz">Comece pelo começo</h2>
             <div class="inputs-novos-quiz">
-                <input class="input-titulo" type="text" placeholder="Titulo do seu quizz">
-                <input class="input-imagem" type="text" placeholder="URL da imagem do seu quizz">
-                <input class="input-qtd-perguntas" type="text" placeholder="Quantidade de perguntas do quizz">
-                <input class="input-qtd-niveis" type="text" placeholder="Quantidade de níveis do quizz">
+                <input value="" class="input-titulo" type="text" placeholder="Titulo do seu quizz">
+                <input value="" class="input-imagem" type="text" placeholder="URL da imagem do seu quizz">
+                <input value="" class="input-qtd-perguntas" type="text" placeholder="Quantidade de perguntas do quizz">
+                <input value="" class="input-qtd-niveis" type="text" placeholder="Quantidade de níveis do quizz">
             </div>
             <button onclick="criarPerguntas()">Prosseguir para as perguntas</button>
         </div>
     `
 }
+
 function criarPerguntas(){
     //const inputImagem = document.querySelector('.input-imagem').value;
-    //ainda nao implementei validaçao de URL
+    //ainda nao implementei a validaçao da URL
     validarTitulo()
     validarQtdPerguntas();
     validarQtdNiveis();
     validarInfos(controleValidacaoInfos);
 }
+
 function validarTitulo(){
     const inputTitulo = document.querySelector('.input-titulo').value;
     if((typeof inputTitulo !=='string' || inputTitulo.length < 20 || inputTitulo.length > 60) === true ){
         controleValidacaoInfos = false;
     }
 }
+
 function validarQtdNiveis(){
     const inputQtdNiveis = document.querySelector('.input-qtd-niveis').value;
     if((isNaN(Number(inputQtdNiveis)) || inputQtdNiveis < 2) === true ){
         controleValidacaoInfos = false;
     }
 }
+
 function validarQtdPerguntas(){
     const inputQtdPerguntas = document.querySelector('.input-qtd-perguntas').value;
     if((isNaN(Number(inputQtdPerguntas)) || inputQtdPerguntas < 3) === true ){
         controleValidacaoInfos = false;
     }
 }
+
 function validarInfos(validacao){
     if(validacao === false){
         alert('Dados inválidos. Por favor, preencha novamente.')
         controleValidacaoInfos = true;
-        informarInfosQuiz();
+        enviarInfosQuiz();
     }else{
-        alert('dados válidos!')
+        const pagQuiz = document.querySelector('.pagina-criando-quiz');
+        const inputQtdPerguntas = document.querySelector('.input-qtd-perguntas').value;
+
+        pagQuiz.innerHTML = `
+        <div class="container-criar-perguntas">
+            <h2 class="instrucoes-criando-perguntas">Crie suas perguntas</h2>
+            <div class="container-perguntas"></div>
+            <button onclick="criarNiveis()">Prosseguir para criar níveis</button>
+        </div>
+        `
+        const containerPerguntas = document.querySelector('.container-perguntas');
+        for(let i = 0; i < Number(inputQtdPerguntas); i++){
+            containerPerguntas.innerHTML += `
+                <div onclick="selecionarPergunta(this)"class="pergunta">
+                    Pergunta ${i+1} 
+                    <ion-icon name="create-outline"></ion-icon>
+                </div>
+            `
+        }
     }
+}
+
+function selecionarPergunta(elemento){
+   const perguntaClicada = elemento;
+   perguntaClicada.classList.add();
+   alert("faz alguma coisa");
 }
 
 let arrayQuiz;
@@ -77,4 +106,3 @@ function quizzRecebido(retorno){
         `
     }
 }
-
